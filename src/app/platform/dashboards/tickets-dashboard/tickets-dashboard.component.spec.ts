1275da71c788da4d4c238694eda8881b5944aed9
Kgothatso Tlaka <mankgwanyane@tlaka.xyz>
2021-01-05 11:17:42 +0200

    Setup: initial project structure

---
+++ src/app/platform/dashboards/tickets-dashboard/tickets-dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TicketsDashboardComponent } from './tickets-dashboard.component';
+
+describe('TicketsDashboardComponent', () => {
+  let component: TicketsDashboardComponent;
+  let fixture: ComponentFixture<TicketsDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TicketsDashboardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TicketsDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/platform/dashboards/tickets-dashboard/tickets-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-dashboard',
+  templateUrl: './tickets-dashboard.component.html',
+  styleUrls: ['./tickets-dashboard.component.scss']
+})
+export class TicketsDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
