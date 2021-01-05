171c6c25283a6ca426c21506a8a2f8e4e00bf913
Kgothatso Tlaka <mankgwanyane@tlaka.xyz>
2021-01-05 19:59:02 +0200

    Add  Progress Spinner
    1. Spinner
    2. Overlay

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,22 @@
 import { MaterialModule } from './modules/material.module';
 import { NgModule } from "@angular/core";
+import { ProgressSpinnerComponent } from './components/loaders/progress-spinner/progress-spinner.component';
+import { AppOverlayModule } from './components/loaders/overlay/overlay.module';
 
 @NgModule({
   imports: [
-     MaterialModule
+     MaterialModule,
+     AppOverlayModule
   ],
   declarations: [
-
-  ],
+  ProgressSpinnerComponent
+],
   entryComponents: [
-
-
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    ProgressSpinnerComponent,
+    AppOverlayModule
   ]
 })
 export class SharedModule { }
